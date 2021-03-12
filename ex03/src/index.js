d04f552959cb76f91254cc338fd15c38d3908d08
OldXary <purisevicharis@gmail.com>
2021-03-12 11:29:37 +0100

    Add ex03

---
+++ ex03/src/index.js
@@ -0,0 +1,11 @@
+function main() {
+    
+ var myString ="FirsLine\n\t\\SecondLine\nThirdLine";
+    
+     
+         return myString;
+     }
+     
+     console.log(main());
+     module.exports = main;
+     
